08d2ee750f699ff8ad6278d1669ee19a377578f0
Axell17 <axel.iv.la.robles@gmail.com>
2024-01-08 00:30:21 -0300

    add register.html

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
 });
 
+app.get('/registro', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
 app.listen(port, () => {
     console.log('Nuestra corre en http://localhost:${port}');
 });
